43f3d76210f7c65b8b4c5d4acb0d43ba1066a1ae
Cihan Fırat <cihan.firat44@gmail.com>
2022-04-04 10:52:06 +0300

    Cember cevre alan hesaplama
    Çemberin değerlerini girerek alan ve çevresini hesaplayabilirsiniz.

---
+++ Cember Alan Cevre Hesaplayıcı/main6.3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	printf("***Cember Alan ve Cevre Hesaplayici***");
+	printf("\n\n");
+	
+	float r,alan,cevre,pi;
+	
+	pi=3.14;
+	printf("Cember Yaricapi: ");
+	scanf("%f",&r);
+	printf("\n");
+	alan=pi*r*r;
+	cevre=2*pi*r;
+	
+	printf("Cemberin Cevresi: %f\n\n",cevre);
+	printf("Cemberin Alani: %f",alan);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
